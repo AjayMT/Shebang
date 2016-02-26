42cf83150e8f638442244aa813a6a5e429148764
Ajay MT <ajay.tatachar@gmail.com>
2016-02-27 01:19:55 +0530

    add MASShortcut and preferences window

---
+++ Shebang/SBPreferencesWindowController.h
@@ -0,0 +1,12 @@
+//
+//  SBPreferencesWindowController.h
+//  Shebang
+//
+//  Created by Ajay Madhusudan on 27/02/16.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SBPreferencesWindowController : NSWindowController
+
+@end
---
+++ Shebang/SBPreferencesWindowController.m
@@ -0,0 +1,22 @@
+//
+//  SBPreferencesWindowController.m
+//  Shebang
+//
+//  Created by Ajay Madhusudan on 27/02/16.
+//
+
+#import "SBPreferencesWindowController.h"
+
+@interface SBPreferencesWindowController ()
+
+@end
+
+@implementation SBPreferencesWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
